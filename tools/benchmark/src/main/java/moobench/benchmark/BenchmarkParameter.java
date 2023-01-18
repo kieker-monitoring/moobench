41955989cd327e30e8a3031edf7519d98fcf8af8
DaGeRe <davidgeorg_reichelt@dagere.de>
2023-01-18 18:27:57 +0100

    Use DurationRecord

--- tools/benchmark/src/main/java/moobench/benchmark/BenchmarkParameter.java
+++ tools/benchmark/src/main/java/moobench/benchmark/BenchmarkParameter.java
@@ -1,91 +1,181 @@
 /**
- * 
  */
 package moobench.benchmark;
 
 import java.io.File;
-
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.converters.FileConverter;
+import kieker.monitoring.core.controller.MonitoringController;
+import kieker.monitoring.core.registry.ControlFlowRegistry;
+import kieker.monitoring.core.registry.SessionRegistry;
+import de.dagere.kopeme.kieker.record.DurationRecord;
 
 /**
  * @author Reiner Jung
- *
  */
 public class BenchmarkParameter {
-	
-	@Parameter(names = { "--total-calls", "-c" }, required = true, description = "Number of total method calls performed.")
-	int totalCalls;
-	
-	@Parameter(names = { "--method-time", "-m" }, required = true, description = "Time a method call takes.")
-	int methodTime;
-	
-	@Parameter(names = { "--total-threads", "-t" }, required = true, description = "Number of threads started.")
-	int totalThreads;
-			
-	@Parameter(names = { "--recursion-depth", "-d" }, required = true, description = "Depth of recursion performed.")
-	int recursionDepth;
 
-	@Parameter(names = { "--output-filename", "-o" }, required = true, converter = FileConverter.class, 
-			description = "Filename of results file. Output is appended if file exists.")
-	File outputFile;
-	
-	@Parameter(names = { "--quickstart", "-q" }, required = false, description = "Skips initial garbage collection.")
-	boolean quickstart;
-	
-	@Parameter(names = { "--force-terminate", "-f" }, required = false, description = "Forces a termination at the end of the benchmark.")
-	boolean forceTerminate;
-    
-	@Parameter(names = { "--runnable", "-r" }, required = false,
-			description = "Class implementing the Runnable interface. run() method is executed before the benchmark starts.")
-	String runnableClassname;
-	
+    private static final kieker.monitoring.core.controller.IMonitoringController _kieker_sourceInstrumentation_controller = kieker.monitoring.core.controller.MonitoringController.getInstance();
+
+    private static final kieker.monitoring.timer.ITimeSource _kieker_sourceInstrumentation_TIME_SOURCE = _kieker_sourceInstrumentation_controller.getTimeSource();
+
+    @Parameter(names = { "--total-calls", "-c" }, required = true, description = "Number of total method calls performed.")
+    int totalCalls;
+
+    @Parameter(names = { "--method-time", "-m" }, required = true, description = "Time a method call takes.")
+    int methodTime;
+
+    @Parameter(names = { "--total-threads", "-t" }, required = true, description = "Number of threads started.")
+    int totalThreads;
+
+    @Parameter(names = { "--recursion-depth", "-d" }, required = true, description = "Depth of recursion performed.")
+    int recursionDepth;
+
+    @Parameter(names = { "--output-filename", "-o" }, required = true, converter = FileConverter.class, description = "Filename of results file. Output is appended if file exists.")
+    File outputFile;
+
+    @Parameter(names = { "--quickstart", "-q" }, required = false, description = "Skips initial garbage collection.")
+    boolean quickstart;
+
+    @Parameter(names = { "--force-terminate", "-f" }, required = false, description = "Forces a termination at the end of the benchmark.")
+    boolean forceTerminate;
+
+    @Parameter(names = { "--runnable", "-r" }, required = false, description = "Class implementing the Runnable interface. run() method is executed before the benchmark starts.")
+    String runnableClassname;
+
     @Parameter(names = { "--application", "-a" }, required = false, description = "Class implementing the MonitoredClass interface.")
     String applicationClassname;
-    
+
     @Parameter(names = { "--benchmark-thread", "-b" }, required = false, description = "Class implementing the BenchmarkingThread interface.")
     String benchmarkClassname;
 
-	public int getTotalCalls() {
-		return totalCalls;
-	}
+    public int getTotalCalls() {
+        final String _kieker_sourceInstrumentation_signature = "public int moobench.benchmark.BenchmarkParameter.getTotalCalls()";;
+              final long _kieker_sourceInstrumentation_tin =_kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+;
+        try {
+            return totalCalls;
+        } finally {
+            // measure after
+final long _kieker_sourceInstrumentation_tout = _kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+_kieker_sourceInstrumentation_controller.newMonitoringRecord(new DurationRecord(_kieker_sourceInstrumentation_signature, _kieker_sourceInstrumentation_tin, _kieker_sourceInstrumentation_tout));
+        }
+    }
 
-	public int getMethodTime() {
-		return methodTime;
-	}
+    public int getMethodTime() {
+        final String _kieker_sourceInstrumentation_signature = "public int moobench.benchmark.BenchmarkParameter.getMethodTime()";;
+              final long _kieker_sourceInstrumentation_tin =_kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+;
+        try {
+            return methodTime;
+        } finally {
+            // measure after
+final long _kieker_sourceInstrumentation_tout = _kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+_kieker_sourceInstrumentation_controller.newMonitoringRecord(new DurationRecord(_kieker_sourceInstrumentation_signature, _kieker_sourceInstrumentation_tin, _kieker_sourceInstrumentation_tout));
+        }
+    }
 
-	public int getTotalThreads() {
-		return totalThreads;
-	}
+    public int getTotalThreads() {
+        final String _kieker_sourceInstrumentation_signature = "public int moobench.benchmark.BenchmarkParameter.getTotalThreads()";;
+              final long _kieker_sourceInstrumentation_tin =_kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+;
+        try {
+            return totalThreads;
+        } finally {
+            // measure after
+final long _kieker_sourceInstrumentation_tout = _kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+_kieker_sourceInstrumentation_controller.newMonitoringRecord(new DurationRecord(_kieker_sourceInstrumentation_signature, _kieker_sourceInstrumentation_tin, _kieker_sourceInstrumentation_tout));
+        }
+    }
 
-	public int getRecursionDepth() {
-		return recursionDepth;
-	}
+    public int getRecursionDepth() {
+        final String _kieker_sourceInstrumentation_signature = "public int moobench.benchmark.BenchmarkParameter.getRecursionDepth()";;
+              final long _kieker_sourceInstrumentation_tin =_kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+;
+        try {
+            return recursionDepth;
+        } finally {
+            // measure after
+final long _kieker_sourceInstrumentation_tout = _kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+_kieker_sourceInstrumentation_controller.newMonitoringRecord(new DurationRecord(_kieker_sourceInstrumentation_signature, _kieker_sourceInstrumentation_tin, _kieker_sourceInstrumentation_tout));
+        }
+    }
 
-	public File getOutputFile() {
-		return outputFile;
-	}
+    public File getOutputFile() {
+        final String _kieker_sourceInstrumentation_signature = "public java.io.File moobench.benchmark.BenchmarkParameter.getOutputFile()";;
+              final long _kieker_sourceInstrumentation_tin =_kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+;
+        try {
+            return outputFile;
+        } finally {
+            // measure after
+final long _kieker_sourceInstrumentation_tout = _kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+_kieker_sourceInstrumentation_controller.newMonitoringRecord(new DurationRecord(_kieker_sourceInstrumentation_signature, _kieker_sourceInstrumentation_tin, _kieker_sourceInstrumentation_tout));
+        }
+    }
 
-	public boolean isQuickstart() {
-		return quickstart;
-	}
+    public boolean isQuickstart() {
+        final String _kieker_sourceInstrumentation_signature = "public boolean moobench.benchmark.BenchmarkParameter.isQuickstart()";;
+              final long _kieker_sourceInstrumentation_tin =_kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+;
+        try {
+            return quickstart;
+        } finally {
+            // measure after
+final long _kieker_sourceInstrumentation_tout = _kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+_kieker_sourceInstrumentation_controller.newMonitoringRecord(new DurationRecord(_kieker_sourceInstrumentation_signature, _kieker_sourceInstrumentation_tin, _kieker_sourceInstrumentation_tout));
+        }
+    }
 
-	public boolean isForceTerminate() {
-		return forceTerminate;
-	}
+    public boolean isForceTerminate() {
+        final String _kieker_sourceInstrumentation_signature = "public boolean moobench.benchmark.BenchmarkParameter.isForceTerminate()";;
+              final long _kieker_sourceInstrumentation_tin =_kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+;
+        try {
+            return forceTerminate;
+        } finally {
+            // measure after
+final long _kieker_sourceInstrumentation_tout = _kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+_kieker_sourceInstrumentation_controller.newMonitoringRecord(new DurationRecord(_kieker_sourceInstrumentation_signature, _kieker_sourceInstrumentation_tin, _kieker_sourceInstrumentation_tout));
+        }
+    }
 
-	public String getRunnableClassname() {
-		return runnableClassname;
-	}
+    public String getRunnableClassname() {
+        final String _kieker_sourceInstrumentation_signature = "public java.lang.String moobench.benchmark.BenchmarkParameter.getRunnableClassname()";;
+              final long _kieker_sourceInstrumentation_tin =_kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+;
+        try {
+            return runnableClassname;
+        } finally {
+            // measure after
+final long _kieker_sourceInstrumentation_tout = _kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+_kieker_sourceInstrumentation_controller.newMonitoringRecord(new DurationRecord(_kieker_sourceInstrumentation_signature, _kieker_sourceInstrumentation_tin, _kieker_sourceInstrumentation_tout));
+        }
+    }
 
-	public String getApplicationClassname() {
-		return applicationClassname;
-	}
+    public String getApplicationClassname() {
+        final String _kieker_sourceInstrumentation_signature = "public java.lang.String moobench.benchmark.BenchmarkParameter.getApplicationClassname()";;
+              final long _kieker_sourceInstrumentation_tin =_kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+;
+        try {
+            return applicationClassname;
+        } finally {
+            // measure after
+final long _kieker_sourceInstrumentation_tout = _kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+_kieker_sourceInstrumentation_controller.newMonitoringRecord(new DurationRecord(_kieker_sourceInstrumentation_signature, _kieker_sourceInstrumentation_tin, _kieker_sourceInstrumentation_tout));
+        }
+    }
 
-	public String getBenchmarkClassname() {
-		return benchmarkClassname;
-	}
-    
-    
-    
+    public String getBenchmarkClassname() {
+        final String _kieker_sourceInstrumentation_signature = "public java.lang.String moobench.benchmark.BenchmarkParameter.getBenchmarkClassname()";;
+              final long _kieker_sourceInstrumentation_tin =_kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+;
+        try {
+            return benchmarkClassname;
+        } finally {
+            // measure after
+final long _kieker_sourceInstrumentation_tout = _kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+_kieker_sourceInstrumentation_controller.newMonitoringRecord(new DurationRecord(_kieker_sourceInstrumentation_signature, _kieker_sourceInstrumentation_tin, _kieker_sourceInstrumentation_tout));
+        }
+    }
 }
--- tools/benchmark/src/main/java/moobench/benchmark/BenchmarkingThreadMilli.java
+++ tools/benchmark/src/main/java/moobench/benchmark/BenchmarkingThreadMilli.java
@@ -1,70 +1,118 @@
-/***************************************************************************
- * Copyright 2014 Kieker Project (http://kieker-monitoring.net)
+/**
+ * ************************************************************************
+ *  Copyright 2014 Kieker Project (http://kieker-monitoring.net)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ***************************************************************************/
-
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * *************************************************************************
+ */
 package moobench.benchmark;
 
 import java.util.concurrent.CountDownLatch;
-
 import moobench.application.MonitoredClass;
+import kieker.monitoring.core.controller.MonitoringController;
+import kieker.monitoring.core.registry.ControlFlowRegistry;
+import kieker.monitoring.core.registry.SessionRegistry;
+import de.dagere.kopeme.kieker.record.DurationRecord;
 
 /**
  * @author Jan Waller
  */
 public final class BenchmarkingThreadMilli implements BenchmarkingThread {
 
-  private final MonitoredClass mc;
-  private final CountDownLatch doneSignal;
-  private final int totalCalls;
-  private final long methodTime;
-  private final int recursionDepth;
-  private final long[] timings;
+    private static final kieker.monitoring.core.controller.IMonitoringController _kieker_sourceInstrumentation_controller = kieker.monitoring.core.controller.MonitoringController.getInstance();
 
-  public BenchmarkingThreadMilli(final MonitoredClass mc, final int totalCalls,
-      final long methodTime, final int recursionDepth, final CountDownLatch doneSignal) {
-    this.mc = mc;
-    this.doneSignal = doneSignal;
-    this.totalCalls = totalCalls;
-    this.methodTime = methodTime;
-    this.recursionDepth = recursionDepth;
-    this.timings = new long[totalCalls];
-  }
+    private static final kieker.monitoring.timer.ITimeSource _kieker_sourceInstrumentation_TIME_SOURCE = _kieker_sourceInstrumentation_controller.getTimeSource();
 
-  public final long[] getTimings() {
-    synchronized (this) {
-      return this.timings;
+    private final MonitoredClass mc;
+
+    private final CountDownLatch doneSignal;
+
+    private final int totalCalls;
+
+    private final long methodTime;
+
+    private final int recursionDepth;
+
+    private final long[] timings;
+
+    public BenchmarkingThreadMilli(final MonitoredClass mc, final int totalCalls, final long methodTime, final int recursionDepth, final CountDownLatch doneSignal) {
+        final String _kieker_sourceInstrumentation_signature = "public new moobench.benchmark.BenchmarkingThreadMilli.<init>(moobench.application.MonitoredClass,int,long,int,java.util.concurrent.CountDownLatch)";;
+              final long _kieker_sourceInstrumentation_tin =_kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+;
+        try {
+            this.mc = mc;
+            this.doneSignal = doneSignal;
+            this.totalCalls = totalCalls;
+            this.methodTime = methodTime;
+            this.recursionDepth = recursionDepth;
+            this.timings = new long[totalCalls];
+        } finally {
+            // measure after
+final long _kieker_sourceInstrumentation_tout = _kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+_kieker_sourceInstrumentation_controller.newMonitoringRecord(new DurationRecord(_kieker_sourceInstrumentation_signature, _kieker_sourceInstrumentation_tin, _kieker_sourceInstrumentation_tout));
+        }
     }
-  }
 
-  public String print(final int index, final String separatorString) {
-    return "" + this.timings[index];
-  }
-
-  public final void run() {
-    long start_ns;
-    long stop_ns;
-    for (int i = 0; i < this.totalCalls; i++) {
-      start_ns = System.currentTimeMillis();
-      this.mc.monitoredMethod(this.methodTime, this.recursionDepth);
-      stop_ns = System.currentTimeMillis();
-      this.timings[i] = stop_ns - start_ns;
-      if ((i % 100000) == 0) {
-        System.out.println(i); // NOPMD (System.out)
-      }
+    public final long[] getTimings() {
+        final String _kieker_sourceInstrumentation_signature = "public final long[] moobench.benchmark.BenchmarkingThreadMilli.getTimings()";;
+              final long _kieker_sourceInstrumentation_tin =_kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+;
+        try {
+            synchronized (this) {
+                return this.timings;
+            }
+        } finally {
+            // measure after
+final long _kieker_sourceInstrumentation_tout = _kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+_kieker_sourceInstrumentation_controller.newMonitoringRecord(new DurationRecord(_kieker_sourceInstrumentation_signature, _kieker_sourceInstrumentation_tin, _kieker_sourceInstrumentation_tout));
+        }
     }
-    this.doneSignal.countDown();
-  }
 
+    public String print(final int index, final String separatorString) {
+        final String _kieker_sourceInstrumentation_signature = "public java.lang.String moobench.benchmark.BenchmarkingThreadMilli.print(int,java.lang.String)";;
+              final long _kieker_sourceInstrumentation_tin =_kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+;
+        try {
+            return "" + this.timings[index];
+        } finally {
+            // measure after
+final long _kieker_sourceInstrumentation_tout = _kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+_kieker_sourceInstrumentation_controller.newMonitoringRecord(new DurationRecord(_kieker_sourceInstrumentation_signature, _kieker_sourceInstrumentation_tin, _kieker_sourceInstrumentation_tout));
+        }
+    }
+
+    public final void run() {
+        final String _kieker_sourceInstrumentation_signature = "public final void moobench.benchmark.BenchmarkingThreadMilli.run()";;
+              final long _kieker_sourceInstrumentation_tin =_kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+;
+        try {
+            long start_ns;
+            long stop_ns;
+            for (int i = 0; i < this.totalCalls; i++) {
+                start_ns = System.currentTimeMillis();
+                this.mc.monitoredMethod(this.methodTime, this.recursionDepth);
+                stop_ns = System.currentTimeMillis();
+                this.timings[i] = stop_ns - start_ns;
+                if ((i % 100000) == 0) {
+                    // NOPMD (System.out)
+                    System.out.println(i);
+                }
+            }
+            this.doneSignal.countDown();
+        } finally {
+            // measure after
+final long _kieker_sourceInstrumentation_tout = _kieker_sourceInstrumentation_TIME_SOURCE.getTime();
+_kieker_sourceInstrumentation_controller.newMonitoringRecord(new DurationRecord(_kieker_sourceInstrumentation_signature, _kieker_sourceInstrumentation_tin, _kieker_sourceInstrumentation_tout));
+        }
+    }
 }
