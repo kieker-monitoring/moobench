95ce0241dc538b3e579d13e814f9d1ac30d3f7b4
DaGeRe <davidgeorg_reichelt@dagere.de>
2023-06-14 21:55:22 +0200

    Do not fail on missing results

--- common-functions.sh
+++ common-functions.sh
@@ -116,12 +116,12 @@ function printIntermediaryResults {
    loop="$1"
    for ((index=0;index<${#TITLE[@]};index+=1)); do
       RESULT_FILE="${RAWFN}-${loop}-${RECURSION_DEPTH}-${index}.csv"
-      checkFile result "${RESULT_FILE}"
+      #checkFile result "${RESULT_FILE}"
       raw_length=`cat "${RESULT_FILE}" | wc -l`
-      if [ "${raw_length}" == "0" ] ; then
-         error "Result file '${RESULT_FILE}' is empty."
-         exit 1
-      fi
+      #if [ "${raw_length}" == "0" ] ; then
+      #   error "Result file '${RESULT_FILE}' is empty."
+      #   exit 1
+      #fi
       info_n "Intermediary results "${TITLE[$index]}" (in ns) "
       cat "${RESULT_FILE}" | awk -F';' '{print $2}' | getSum
    done
