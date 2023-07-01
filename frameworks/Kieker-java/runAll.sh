1ee5604d71cf4009bc208a479f484df5bb1f4b75
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2023-07-01 21:42:16 +0200

    Add METHOD_TIME experiment

--- frameworks/Kieker-java/runAll.sh
+++ frameworks/Kieker-java/runAll.sh
@@ -1,6 +1,6 @@
 JAVA_FOLDER=$(java -version 2>&1 | grep version | tr " " "_" | tr -d "\"")
 mkdir $JAVA_FOLDER
-export NUM_OF_LOOPS=10; export SLEEP_TIME=1;
+export NUM_OF_LOOPS=30; export SLEEP_TIME=5;
 echo "Running Depth Experiments"
 for depth in 2 10 20 50 75 100 200 300 400 500; do echo -n "$depth.. "; export RECURSION_DEPTH=$depth; ./benchmark.sh &> $JAVA_FOLDER/depth_$depth.txt; done
 unset RECURSION_DEPTH
@@ -19,3 +19,7 @@ echo
 echo "Running Dummy Loops Experiments"
 for loops in {1..20}; do echo -n "$loops.. "; export DUMMY_LOOPS=$loops; ./benchmark.sh &> $JAVA_FOLDER/dummy_$loops.txt; done
 unset DUMMY_LOOPS
+
+echo "Running METHOD_TIME Experiments"
+for mtime in 1 10 100 1000 10000; do echo -n "$mtime.. "; export METHOD_TIME=$mtime; ./benchmark.sh &> $JAVA_FOLDER/time_$mtime.txt; done
+unset METHOD_TIME
