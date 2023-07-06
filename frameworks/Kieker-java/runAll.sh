ab9521665b0bb9625ff4720be18bafc2fbbc2497
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2023-07-06 11:43:24 +0200

    Add more METHOD_TIME options

--- frameworks/Kieker-java/runAll.sh
+++ frameworks/Kieker-java/runAll.sh
@@ -22,5 +22,5 @@ unset DUMMY_LOOPS
 
 echo
 echo "Running METHOD_TIME Experiments"
-for mtime in 1 10 100 1000 10000; do echo -n "$mtime.. "; export METHOD_TIME=$mtime; ./benchmark.sh &> $JAVA_FOLDER/time_$mtime.txt; done
+for mtime in 1 10 100 200 300 400 500 1000 10000; do echo -n "$mtime.. "; export METHOD_TIME=$mtime; ./benchmark.sh &> $JAVA_FOLDER/time_$mtime.txt; done
 unset METHOD_TIME
