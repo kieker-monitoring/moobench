740a319f8d31ed784017aaf70d0913ee34524663
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2023-07-14 21:50:44 +0200

    Create diffs from both views

--- frameworks/Kieker-java/createGraphs.sh
+++ frameworks/Kieker-java/createGraphs.sh
@@ -3,4 +3,5 @@ perf script -i perf_2_1.data | $FLAME_HOME/stackcollapse-perf.pl > nologging.fol
 cat nologging.folded | $FLAME_HOME/flamegraph.pl > nologging.svg
 perf script -i perf_4_1.data | $FLAME_HOME/stackcollapse-perf.pl > binary.folded
 cat binary.folded | $FLAME_HOME/flamegraph.pl > binary.svg
-$FLAME_HOME/difffolded.pl nologging.folded binary.folded | $FLAME_HOME/flamegraph.pl > diff.svg
+$FLAME_HOME/difffolded.pl nologging.folded binary.folded | $FLAME_HOME/flamegraph.pl > diffNoLogging.svg
+$FLAME_HOME/difffolded.pl binary.folded nologging.folded | $FLAME_HOME/flamegraph.pl > diffBinary.svg
