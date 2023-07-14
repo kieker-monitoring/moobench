a9295564c2a4c4ad1f9129239fd8d27878f883ec
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2023-07-14 15:54:30 +0200

    Cleanup data

--- frameworks/Kieker-java/runAll.sh
+++ frameworks/Kieker-java/runAll.sh
@@ -1,14 +1,21 @@
 
 function run {
 	time=$1
-	export NUM_OF_LOOPS=1; export METHOD_TIME=$time; ./benchmark.sh
-	./createGraphs.sh
+	
+	echo "Running METHOD_TIME $time"
+	
 	folder=METHOD_TIME_$time
 	mkdir $folder
+	
+	export NUM_OF_LOOPS=1; export METHOD_TIME=$time; ./benchmark.sh &> $folder/log.txt
+	./createGraphs.sh
 	mv results $folder
-	mv *svg $folder
+	mv *.folded $folder
+	mv *.svg $folder
 	mv perf* $folder
 }
 
 run 1
 run 2000
+run 10000
+run 100000
