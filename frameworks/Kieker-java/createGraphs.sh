4eb14dcb74b58387c598988a40717099a490c661
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2023-07-14 13:10:43 +0200

    Add createGraphs.sh

---
+++ frameworks/Kieker-java/createGraphs.sh
@@ -0,0 +1,6 @@
+sudo chown $(whoami):$(whoami) perf*
+perf script -i perf_2_1.data | $FLAME_HOME/stackcollapse-perf.pl > nologging.folded
+cat nologging.folded | $FLAME_HOME/flamegraph.pl > nologging.svg
+perf script -i perf_4_1.data | $FLAME_HOME/stackcollapse-perf.pl > binary.folded
+cat binary.folded | $FLAME_HOME/flamegraph.pl > binary.svg
+$FLAME_HOME/difffolded.pl nologging.folded binary.folded | $FLAME_HOME/flamegraph.pl > diff.svg
