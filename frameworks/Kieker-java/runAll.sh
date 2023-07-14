901aea64e89e3a0c3251db14bc19279bf96a0e3e
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2023-07-14 12:05:06 +0200

    Print current memory

--- frameworks/Kieker-java/runAll.sh
+++ frameworks/Kieker-java/runAll.sh
@@ -6,7 +6,8 @@ echo
 echo "Running METHOD_TIME Experiments"
 for space in "-Xmx1000m" "-Xmx1500m" "-Xmx2000m" "-Xmx2500m" "-Xmx3000m" "-Xmx3500m"
 do
-        export SPACE=$space
+        echo "Memory: $space"
+	export SPACE=$space
         for mtime in 1 10 100 1000 2000 10000; do echo -n "$mtime.. "; export METHOD_TIME=$mtime; ./benchmark.sh &> $JAVA_FOLDER/time_$mtime"_$space".txt; done
 done
 unset METHOD_TIME
