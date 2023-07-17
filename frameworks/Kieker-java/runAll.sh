176adbc117cb627e7ecbb2eb383238d2e12e7e7f
DaGeRe <davidgeorg_reichelt@dagere.de>
2023-07-17 11:13:37 +0200

    Also run empty space configuration (-> no parameters)

--- frameworks/Kieker-java/runAll.sh
+++ frameworks/Kieker-java/runAll.sh
@@ -4,7 +4,7 @@ export NUM_OF_LOOPS=30; export SLEEP_TIME=5;
 
 echo
 echo "Running METHOD_TIME Experiments"
-for space in "-Xmx1000m" "-Xmx1500m" "-Xmx2000m" "-Xmx2500m" "-Xmx3000m" "-Xmx3500m"
+for space in " " "-Xmx1000m" "-Xmx1500m" "-Xmx2000m" "-Xmx2500m" "-Xmx3000m" "-Xmx3500m"
 do
         echo "Memory: $space"
 	export SPACE=$space
