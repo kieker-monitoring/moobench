ea18ca089ead3c10c079896fa65eb8b54899b64c
DaGeRe <davidgeorg_reichelt@dagere.de>
2023-07-18 10:18:30 +0200

    Adapt parameters

--- frameworks/Kieker-java/runAll.sh
+++ frameworks/Kieker-java/runAll.sh
@@ -4,5 +4,5 @@ export NUM_OF_LOOPS=30; export SLEEP_TIME=5;
 
 echo
 echo "Running DUMMY_LOOPS (in MonitoredClassLoops) Experiments"
-for loops in {1..20}; do echo -n "$loops.. "; export DUMMY_LOOPS=$loops; ./benchmark.sh &> $JAVA_FOLDER/classDummy_$loops.txt; done
+for loops in 1 10 100 500 1000 2000 5000; do echo -n "$loops.. "; export DUMMY_LOOPS=$loops; ./benchmark.sh &> $JAVA_FOLDER/classDummy_$loops.txt; done
 unset DUMMY_LOOPS
