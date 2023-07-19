8a1a57efa878ab09d0dfb9291ef29b8eacb3511c
DaGeRe <davidgeorg_reichelt@dagere.de>
2023-07-19 17:32:19 +0200

    Run only 10 loops as default in MooBench

--- frameworks/Kieker-java/runAll.sh
+++ frameworks/Kieker-java/runAll.sh
@@ -1,6 +1,6 @@
 JAVA_FOLDER=$(java -version 2>&1 | grep version | tr " " "_" | tr -d "\"")
 mkdir $JAVA_FOLDER
-export NUM_OF_LOOPS=30; export SLEEP_TIME=5;
+export NUM_OF_LOOPS=10; export SLEEP_TIME=5;
 
 echo "Running Depth Experiments"
 for depth in 2 10 20 50 75 100; do echo -n "$depth.. "; export RECURSION_DEPTH=$depth; ./benchmark.sh &> $JAVA_FOLDER/depth_$depth.txt; done
