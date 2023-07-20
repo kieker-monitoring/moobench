e862da05788de36049d735181f2611d2f55c199e
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2023-07-20 21:58:39 +0200

    Also store zip results

--- frameworks/Kieker-java/runAll.sh
+++ frameworks/Kieker-java/runAll.sh
@@ -3,6 +3,12 @@ mkdir $JAVA_FOLDER
 export NUM_OF_LOOPS=10; export SLEEP_TIME=5;
 
 echo "Running Depth Experiments"
-for depth in 2 10 20 50 75 100; do echo -n "$depth.. "; export RECURSION_DEPTH=$depth; ./benchmark.sh &> $JAVA_FOLDER/depth_$depth.txt; done
+for depth in 2 10 20 50 75 100
+do 
+	echo -n "$depth.. "
+	export RECURSION_DEPTH=$depth
+	./benchmark.sh &> $JAVA_FOLDER/depth_$depth.txt
+	mv results/results.zip $JAVA_FOLDER/depth_$depth.zip
+done
 unset RECURSION_DEPTH
 
