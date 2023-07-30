126e9a9db5d85766b237861cfd1d7011275ccbf5
DaGeRe <davidgeorg_reichelt@dagere.de>
2023-07-30 20:05:18 +0200

    Add runAndStore function

--- frameworks/Kieker-java/runAll.sh
+++ frameworks/Kieker-java/runAll.sh
@@ -1,4 +1,11 @@
 JAVA_FOLDER=$(java -version 2>&1 | grep version | tr " " "_" | tr -d "\"")
+
+function runAndStore {
+	filePrefix=$1
+	./benchmark.sh &> $JAVA_FOLDER/$filePrefix.txt
+	mv results/results.zip $JAVA_FOLDER/$filePrefix.zip
+}
+
 mkdir $JAVA_FOLDER
 export NUM_OF_LOOPS=10; export SLEEP_TIME=5;
 
@@ -7,8 +14,7 @@ for depth in 2 10 20 50 75 100
 do 
 	echo -n "$depth.. "
 	export RECURSION_DEPTH=$depth
-	./benchmark.sh &> $JAVA_FOLDER/depth_$depth.txt
-	mv results/results.zip $JAVA_FOLDER/depth_$depth.zip
+	runAndStore depth_$depth
 done
 unset RECURSION_DEPTH
 
