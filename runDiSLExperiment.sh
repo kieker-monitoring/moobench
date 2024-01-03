2e579794eedd005d879fc7d45dc6d15c68f0ac00
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-01-03 15:59:18 +0100

    Add script for running complete DiSL experiment

---
+++ runDiSLExperiment.sh
@@ -0,0 +1,32 @@
+if [ -z "${DISL_HOME}" ]; then
+        echo "\$DISL_HOME needs to be defined."
+        exit 1
+fi
+
+
+echo "======="
+echo "SI"
+git checkout optimization-sourceInstrumentation
+./setup.sh &> setup_si.txt
+cd frameworks/Kieker-java
+./runExponentialSizes.sh
+mv exp-results ../SI-exp-results
+cd ../../
+
+echo "======="
+echo "AspectJ"
+git checkout Kieker-1.15.2-OperationExecutionRecord
+./setup.sh &> setup_aspectj.txt
+cd frameworks/Kieker-java
+./runExponentialSizes.sh
+mv exp-results ../AspectJ-exp-results
+cd ../../
+
+echo "======="
+echo "DiSL"
+git checkout disl
+./setup.sh &> setup_disl.txt
+cd frameworks/Kieker-java-DiSL
+./runExponentialSizes.sh
+mv exp-results ../DiSL-exp-results
+cd ../../
