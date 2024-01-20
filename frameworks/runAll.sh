83370dbc1154cb9edeb23ed0b1db4080343aa6f5
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-01-20 14:13:54 +0100

    Add build hint

--- frameworks/runAll.sh
+++ frameworks/runAll.sh
@@ -26,7 +26,7 @@ do
 	AGENT_RAW_PATH="../../../kieker/build/libs/kieker-2.0.0-SNAPSHOT-$agent_type.jar"
 	if [ ! -f "${AGENT_RAW_PATH}" ]
 	then
-		error "Kieker agent for $agent_type in $AGENT_RAW_PATH not present; please build Kieker with an appropriate branch."
+		error "Kieker agent for $agent_type in $AGENT_RAW_PATH not present; please build Kieker with an appropriate branch. (./gradlew mainJar bytebuddyJar aspectjJar javassistJar -x test -x check)"
 		ls
 		exit 1
 	fi
