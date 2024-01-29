b0290387f7e2f821b5cc357d00cbf5581a92b156
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-01-29 16:04:37 +0100

    Make sizes configurable also for AspectJ

--- frameworks/OpenTelemetry-java/runExponentialSizes.sh
+++ frameworks/OpenTelemetry-java/runExponentialSizes.sh
@@ -14,6 +14,13 @@ else
 	exit 1
 fi
 
+if [ -z "$EXPONENTIAL_SIZES" ]
+then
+	EXPONENTIAL_SIZES="2 4 8 16 32 64 128"
+	echo "Setting default exponential sizes $EXPONENTIAL_SIZES"
+fi
+echo "Running exponential sizes: $EXPONENTIAL_SIZES"
+
 RESULTS_DIR="${BASE_DIR}/exp-results"
 
 #
@@ -28,7 +35,7 @@ checkDirectory RESULTS_DIR "${RESULTS_DIR}" create
 
 cd "${BASE_DIR}"
 
-for depth in 2 4 8 16 32 64 128
+for depth in $EXPONENTIAL_SIZES
 do
 	export RECURSION_DEPTH=$depth
 	info "Running $depth"
