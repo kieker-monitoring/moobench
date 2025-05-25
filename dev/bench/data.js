69227fd9de19974c54782c2e5d909fc1b9526380
github-action-benchmark <github@users.noreply.github.com>
2025-05-25 05:56:36 +0000

    add Pinpoint-java (customSmallerIsBetter) benchmark result for 6b0a185cacf0f3d26ae6b0133990b820b16a3ff2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748151535701,
+  "lastUpdate": 1748152596155,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -128502,6 +128502,52 @@ window.BENCHMARK_DATA = {
             "value": 6715.39,
             "unit": "ns",
             "range": 248.585
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "6b0a185cacf0f3d26ae6b0133990b820b16a3ff2",
+          "message": "Add debug output before printing result",
+          "timestamp": "2025-04-17T19:45:24Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/6b0a185cacf0f3d26ae6b0133990b820b16a3ff2"
+        },
+        "date": 1748152588110,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.8833,
+            "unit": "ns",
+            "range": 0.0899076
+          },
+          {
+            "name": "Pinpoint Deactivated",
+            "value": 92.4822,
+            "unit": "ns",
+            "range": 3.90911
+          },
+          {
+            "name": "Pinpoint Basic",
+            "value": 11641.3,
+            "unit": "ns",
+            "range": 749.004
+          },
+          {
+            "name": "Pinpoint Sampling",
+            "value": 6751.56,
+            "unit": "ns",
+            "range": 333.458
           }
         ]
       }
