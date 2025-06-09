32e4034bc8dfa7190e27e471db7e37a3e257ca66
github-action-benchmark <github@users.noreply.github.com>
2025-06-09 04:23:48 +0000

    add Skywalking-java (customSmallerIsBetter) benchmark result for 3ca1b01bf6a740d75c9f71e163b9a356f49c2359

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749361523659,
+  "lastUpdate": 1749443028783,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -133772,6 +133772,52 @@ window.BENCHMARK_DATA = {
             "value": 90.9431,
             "unit": "ns",
             "range": 0.0912288
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "3ca1b01bf6a740d75c9f71e163b9a356f49c2359",
+          "message": "Update to gradle 8.14.1",
+          "timestamp": "2025-05-25T17:03:30Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/3ca1b01bf6a740d75c9f71e163b9a356f49c2359"
+        },
+        "date": 1749443016401,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.9916,
+            "unit": "ns",
+            "range": 0.120511
+          },
+          {
+            "name": "Skywalking instrumentation",
+            "value": 4464.45,
+            "unit": "ns",
+            "range": 90.0576
+          },
+          {
+            "name": "Skywalking 20Hz Sampling",
+            "value": 4461.2,
+            "unit": "ns",
+            "range": 76.5078
+          },
+          {
+            "name": "Skywalking no data collection",
+            "value": 90.9398,
+            "unit": "ns",
+            "range": 0.124105
           }
         ]
       }
