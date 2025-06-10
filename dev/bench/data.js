44b42c29c326fb0a66606e3f90b63865250fc3c8
github-action-benchmark <github@users.noreply.github.com>
2025-06-10 04:42:23 +0000

    add ElasticAPM-java (customSmallerIsBetter) benchmark result for 3ca1b01bf6a740d75c9f71e163b9a356f49c2359

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749530254497,
+  "lastUpdate": 1749530543434,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -115298,6 +115298,52 @@ window.BENCHMARK_DATA = {
             "value": 8525.08,
             "unit": "ns",
             "range": 1029.72
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "3ca1b01bf6a740d75c9f71e163b9a356f49c2359",
+          "message": "Update to gradle 8.14.1",
+          "timestamp": "2025-05-25T17:03:30Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/3ca1b01bf6a740d75c9f71e163b9a356f49c2359"
+        },
+        "date": 1749530531581,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.9416,
+            "unit": "ns",
+            "range": 0.0830277
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 339.052,
+            "unit": "ns",
+            "range": 8.82486
+          },
+          {
+            "name": "Regular Writing",
+            "value": 8317.49,
+            "unit": "ns",
+            "range": 1106.45
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 8229.48,
+            "unit": "ns",
+            "range": 1113.01
           }
         ]
       }
