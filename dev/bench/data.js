7fb07779b4689ae4801e387beef18bd334da3f49
github-action-benchmark <github@users.noreply.github.com>
2025-06-16 04:47:13 +0000

    add OpenTelemetry-java (customSmallerIsBetter) benchmark result for 3ca1b01bf6a740d75c9f71e163b9a356f49c2359

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750049205783,
+  "lastUpdate": 1750049233557,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -48468,6 +48468,52 @@ window.BENCHMARK_DATA = {
             "value": 4087.24,
             "unit": "ns",
             "range": 112.035
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "3ca1b01bf6a740d75c9f71e163b9a356f49c2359",
+          "message": "Update to gradle 8.14.1",
+          "timestamp": "2025-05-25T17:03:30Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/3ca1b01bf6a740d75c9f71e163b9a356f49c2359"
+        },
+        "date": 1750049220951,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.9349,
+            "unit": "ns",
+            "range": 0.148882
+          },
+          {
+            "name": "No logging",
+            "value": 2655.58,
+            "unit": "ns",
+            "range": 96.749
+          },
+          {
+            "name": "Zipkin",
+            "value": 4482.84,
+            "unit": "ns",
+            "range": 206.046
+          },
+          {
+            "name": "Prometheus",
+            "value": 4017.09,
+            "unit": "ns",
+            "range": 91.9573
           }
         ]
       }
