fb814b09bb0d5163f09c86e25103bffd98365b29
github-action-benchmark <github@users.noreply.github.com>
2025-06-23 04:49:17 +0000

    add ElasticAPM-java (customSmallerIsBetter) benchmark result for 3ca1b01bf6a740d75c9f71e163b9a356f49c2359

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750654148347,
+  "lastUpdate": 1750654157633,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -117322,6 +117322,52 @@ window.BENCHMARK_DATA = {
             "value": 8560.34,
             "unit": "ns",
             "range": 1217.51
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "3ca1b01bf6a740d75c9f71e163b9a356f49c2359",
+          "message": "Update to gradle 8.14.1",
+          "timestamp": "2025-05-25T17:03:30Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/3ca1b01bf6a740d75c9f71e163b9a356f49c2359"
+        },
+        "date": 1750654142869,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.9841,
+            "unit": "ns",
+            "range": 0.246484
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 327.961,
+            "unit": "ns",
+            "range": 21.2531
+          },
+          {
+            "name": "Regular Writing",
+            "value": 8350.13,
+            "unit": "ns",
+            "range": 1032.06
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 8639.71,
+            "unit": "ns",
+            "range": 823.564
           }
         ]
       }
