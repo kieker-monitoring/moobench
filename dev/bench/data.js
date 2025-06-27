bdade75cd47c2c2a191237475d2e68b61a6c70db
github-action-benchmark <github@users.noreply.github.com>
2025-06-27 04:40:06 +0000

    add Kieker-java (customSmallerIsBetter) benchmark result for 3ca1b01bf6a740d75c9f71e163b9a356f49c2359

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750917516962,
+  "lastUpdate": 1750999206861,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -26218,6 +26218,52 @@ window.BENCHMARK_DATA = {
             "value": 3812.16,
             "unit": "ns",
             "range": 48.916
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "3ca1b01bf6a740d75c9f71e163b9a356f49c2359",
+          "message": "Update to gradle 8.14.1",
+          "timestamp": "2025-05-25T17:03:30Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/3ca1b01bf6a740d75c9f71e163b9a356f49c2359"
+        },
+        "date": 1750999192567,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.9277,
+            "unit": "ns",
+            "range": 0.26962
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 440.295,
+            "unit": "ns",
+            "range": 4.68785
+          },
+          {
+            "name": "No logging",
+            "value": 1839.05,
+            "unit": "ns",
+            "range": 7.06079
+          },
+          {
+            "name": "Binary file",
+            "value": 3752.42,
+            "unit": "ns",
+            "range": 52.3653
           }
         ]
       }
