4e6e70323ccb0fd2013ca772abd84561f185a5f6
github-action-benchmark <github@users.noreply.github.com>
2025-07-02 05:43:26 +0000

    add inspectIT-java (customSmallerIsBetter) benchmark result for 3ca1b01bf6a740d75c9f71e163b9a356f49c2359

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751431518866,
+  "lastUpdate": 1751435006181,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -131554,6 +131554,58 @@ window.BENCHMARK_DATA = {
             "value": 9706.53,
             "unit": "ns",
             "range": 196.573
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "3ca1b01bf6a740d75c9f71e163b9a356f49c2359",
+          "message": "Update to gradle 8.14.1",
+          "timestamp": "2025-05-25T17:03:30Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/3ca1b01bf6a740d75c9f71e163b9a356f49c2359"
+        },
+        "date": 1751434989497,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 90.8779,
+            "unit": "ns",
+            "range": 0.078786
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 5440.42,
+            "unit": "ns",
+            "range": 267.299
+          },
+          {
+            "name": "No logging",
+            "value": 15711.9,
+            "unit": "ns",
+            "range": 294.701
+          },
+          {
+            "name": "Zipkin",
+            "value": 17151.4,
+            "unit": "ns",
+            "range": 252.59
+          },
+          {
+            "name": "Prometheus",
+            "value": 9688.63,
+            "unit": "ns",
+            "range": 205.7
           }
         ]
       }
