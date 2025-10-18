4ac230f512a7d8623b6b585b1c0d86b9f865319f
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2025-10-18 16:48:28 +0200

    Support depth and threads scaling

--- frameworks/benchmarkDepthScaling.sh
+++ frameworks/benchmarkDepthScaling.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
 
 function runAll {
+	MODE=$1
 	start=$(pwd)
 	for benchmark in inspectIT-java OpenTelemetry-java Kieker-java Scouter-java elasticapm-java pinpoint-java
 	do
-		echo "Benchmarking Depth Scaling $benchmark"
-		runDepthScaling $benchmark
+		echo "Running Depth Scaling $benchmark"
+		if [ "$MODE" = "DEPTH" ]; then
+			runDepthScaling "$benchmark"
+		else
+			runThreadScaling "$benchmark"
+		fi
 	done
+}
+
+function runThreadScaling {
+	benchmark=$1
+	cd "${benchmark}"
+
+	RESULTS_DIR="parallel-results-${benchmark}"
+	checkDirectory RESULTS_DIR "${RESULTS_DIR}" create
+	
+	for threads in 1 2 3 4
+	do
+		export THREADS=$threads
+		echo "Running $threads"
+		echo "REsults to "${RESULTS_DIR}/parallel_$threads.txt
+		./benchmark.sh &> ${RESULTS_DIR}/parallel_$threads.txt
+		if [ ! -f results-$benchmark/results.zip ]
+		then
+			echo "File results-$benchmark/results.zip missing; aborting"
+			exit 1
+		fi
+		mv results-$benchmark/results.zip ${RESULTS_DIR}/results-$RECURSION_DEPTH.zip
+	done
+	
+	cd "${start}"
 }
 
 function runDepthScaling {
@@ -20,7 +49,7 @@ function runDepthScaling {
 	do
 		export RECURSION_DEPTH=$depth
 		echo "Running $depth"
-		./benchmark.sh &> ${RESULTS_DIR}/$depth.txt
+		./benchmark.sh &> ${RESULTS_DIR}/depth_$depth.txt
 		if [ ! -f results-$benchmark/results.zip ]
 		then
 			echo "File results-$benchmark/results.zip missing; aborting"
@@ -50,15 +79,29 @@ else
 fi
 
 if [[ -z "$1" ]]; then
-	echo "Usage: $0 <ALL|foldername>"
+	echo "Usage: $0 <ALL|foldername> <DEPTH|THREADS>"
 	exit 1
+fi
+
+MODE=${2:-DEPTH}
+
+echo "Running mode $MODE"
+
+if [[ -z "$RECURSION_DEPTH" ]]
+then
+	echo "Warning: \$RECURSION_DEPTH is set to $RECURSION_DEPTH"
 fi
 
 case "$1" in
 	ALL)
-		runAll
+		runAll $MODE
 	;;
 	*)
-		runSingle "$1"
+		benchmark=$1
+		if [ "$MODE" = "DEPTH" ]; then
+			runDepthScaling "$benchmark"
+		else
+			runThreadScaling "$benchmark"
+		fi
 	;;
 esac
