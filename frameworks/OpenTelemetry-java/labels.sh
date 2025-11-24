997f7bbae62b96e826391389a6d9aedab394bc8c
DaGeRe <davidgeorg_reichelt@dagere.de>
2025-11-24 15:52:21 +0100

    Fix labels

--- frameworks/OpenTelemetry-java/labels.sh
+++ frameworks/OpenTelemetry-java/labels.sh
@@ -2,10 +2,6 @@ TITLE[0]="No instrumentation"
 TITLE[1]="No logging"
 TITLE[2]="Logging"
 TITLE[3]="Zipkin"
-MACHINE_TYPE=`uname -m`; 
-if [ ${MACHINE_TYPE} == 'x86_64' ]
-then
-	TITLE[4]="Prometheus"
-	#TITLE[5]="OpenTelemetry Jaeger"
-fi
+TITLE[4]="Prometheus"
+TITLE[5]="OpenTelemetry Jaeger"
 TITLE[6]="Zipkin+Cassandra"
