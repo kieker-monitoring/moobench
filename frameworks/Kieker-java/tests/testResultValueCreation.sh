85802bc3cd8c86da9a01a8f1bebce2b2aece635d
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2026-04-21 12:56:25 +0200

    benchmark.sh -> start.sh

--- frameworks/Kieker-java/tests/testResultValueCreation.sh
+++ frameworks/Kieker-java/tests/testResultValueCreation.sh
@@ -16,7 +16,7 @@ setup() {
   export NUM_OF_LOOPS=1 
   export TOTAL_NUM_OF_CALLS=10 
   export RECURSION_DEPTH=10 
-  ./benchmark.sh 
+  run ./start.sh
 
   cd results-Kieker-java/
   count=$(ls *.csv 2>/dev/null | wc -l)
